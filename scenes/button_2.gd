b0109c680c62d3122a75c3f2d4418fcec54da6b5
lumpywork <juliancarter11804@gmail.com>
2025-11-20 22:35:59 -0600

    Add files via upload
    made the settings and the pause menu as well exit button the menu

--- scenes/button_2.gd
+++ scenes/button_2.gd
@@ -4,9 +4,11 @@ extends Button
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-func _pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
+ 
+func _pressed() -> void:
+	get_tree().quit()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
--- scripts/coin.gd
+++ scripts/coin.gd
@@ -4,6 +4,6 @@ extends Area2D
 
 
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(_body: Node2D) -> void:
 	print("+1 coin!") # Replace with function body.
 	queue_free()
