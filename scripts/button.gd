257f3ec6944f6033e5150ae683fe6744acd901e2
jjthejet05 <70729895+jjthejet05@users.noreply.github.com>
2025-11-23 12:19:10 -0600

    Add files via upload

--- scripts/button.gd
+++ scripts/button.gd
@@ -1,12 +1,4 @@
 extends Button
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 func _pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
