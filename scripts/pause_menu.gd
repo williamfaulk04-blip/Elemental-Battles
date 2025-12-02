883f46d6a9ce5132c82e8d4a0dc1adda68e4a25a
Will <williamfaulk04@gmail.com>
2025-12-01 19:16:16 -0600

    Update p2
    adding the rest of the my changes to my branch before pushing to master branch

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -3,27 +3,37 @@ extends Control
 
 func resume():
 	get_tree().paused = false
-	$AnimationPlayerl.play_backwards("blur")
+	$AnimationPlayer.play_backwards("blur")
 	
 	
 func pause():
 	get_tree().paused = true
 	$AnimationPlayer.play("blur")
 	
-func Esc():
-	if Input.is_action_just_pressed("escape") and get_tree().paused == false:
-		pause()
-	elif Input.is_action_just_pressed("escape") and get_tree().paused == true:
+func _input(event):
+		if Input.is_action_just_pressed("escape"):
+			testEsc()
+	
+func testEsc():
+		if !get_tree().paused:
+			pause() 
+			print("pause")
+		
+
+
+func _on_button_pressed():
+	if get_tree().paused:
 		resume()
 
 
-func _on_resume_pressed() -> void:
-	resume()
+func _on_button_2_pressed():
+	if get_tree().paused:
+		resume()
+		get_tree().reload_current_scene()
+	
 
-
-func _on_restart_pressed() -> void:
-	get_tree().reload_current_scene()
-
-
-func _on_exit_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/Menu.tscn")
+func _on_button_3_pressed():
+	if get_tree().paused:
+		resume()
+		get_tree().change_scene_to_file("res://scenes/Menu.tscn")
+	
